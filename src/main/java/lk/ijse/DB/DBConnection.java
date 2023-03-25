e67e0df761b4b0f84dc0d3b505350c84bb66394e
TGDRangaka <tgdilshanrangaka2002@gmail.com>
2023-03-25 21:02:07 +0530

    0.0.1

---
+++ src/main/java/lk/ijse/DB/DBConnection.java
@@ -0,0 +1,36 @@
+package lk.ijse.DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+
+    private final static String URL ="jdbc:mysql://localhost:3306/ThogaKade";
+    private final static Properties props = new Properties();
+
+    private Connection connection;
+
+    {
+        props.setProperty("user", "root");
+        props.setProperty("password", "1234");
+    }
+
+    private DBConnection() throws SQLException {
+        connection = DriverManager.getConnection(URL, props);
+    }
+
+    public static DBConnection getInstance() throws SQLException {
+        if(dbConnection == null){
+            return dbConnection = new DBConnection();
+        }else{
+            return dbConnection;
+        }
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}
